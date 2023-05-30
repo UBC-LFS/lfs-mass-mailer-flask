d529b3fcca323d964e080ddf671c945529cc38cf
DonaldKLee <Donald3010@outlook.com>
2023-05-30 13:05:10 -0700

    sending emails work

---
+++ sendEmail.py
@@ -0,0 +1,29 @@
+from smtplib import SMTP
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
+USER = os.getenv("USER")
+PASSWORD = os.getenv("PASSWORD")
+
+def sendEmail(to):
+    try:
+        emailService = SMTP(host=HOST, port=PORT)
+        emailService.ehlo()
+        emailService.starttls()
+        emailService.login(user=USER, password=PASSWORD, initial_response_ok=True)
+        emailService.sendmail(
+            USER,
+            to,
+            'Subject: Test \n\n This is a test email!')
+        emailService.quit()
+        print("Successfully sent email")
+
+    except Exception as e:
+       print("\nError:\n" + str(e) + "\n")
+
+to = input("Who would you like to send this email to?: ")
+sendEmail(to)
