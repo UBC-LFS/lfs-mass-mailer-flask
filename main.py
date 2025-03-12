74b92623483529c8876e9bbd763b69682a57b06b
Simon Lin <psymon704@gmail.com>
2025-03-12 09:51:57 -0700

    cc added to logs

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask_login import LoginManager, current_user
 from flask_ldap3_login import LDAP3LoginManager
 import secrets
 import sendEmail
+import writeLog
 import time
 
 load_dotenv()
@@ -47,59 +48,6 @@ ldap_manager.init_config(app.config)
 # Create a dictionary to store the users in when they authenticate
 # This example stores users in memory.
 users = {}
-
-log_export_path = os.getenv("LOG_EXPORT_PATH") + "/"
-log_export_path = log_export_path.replace("\\", "/")
-
-
-def write_log(user, subject, message, receivers, failedReceivers):
-    # TODO - add CCs to log
-    time_file_str = time.strftime("%Y-%m-%d_%H-%M-%S")
-
-    if not os.path.isdir(log_export_path):
-        print("invalid export path")
-        return "invalid export path"
-
-    try:
-        file_name = log_export_path + time_file_str + ".txt"
-
-        f = open(file_name, "a")  # open file in append mode
-
-        date_str = time.strftime("%Y/%m/%d")
-        time_str = time.strftime("%H:%M:%S")
-
-        sender_address = os.getenv("ACCOUNT_USER")
-        if sender_address is None or sender_address == "":
-            sender_address = os.getenv("ACCOUNT_USER_RELAY")
-
-        message_parsed = (
-            message.replace("<p>", "").replace("</p>", "").replace("<br>", "\n")
-        )
-
-        f.write("Date (YYYY/MM/DD): " + date_str + "\n")
-        f.write("Time (HH:MM:SS): " + time_str + "\n\n")
-        f.write("Sender: " + user + "\n")
-        f.write("Sender Email: " + sender_address + "\n\n")
-        f.write("Subject: " + subject + "\n")
-        f.write(
-            "Message: \n------------------------BEGIN_MESSAGE------------------------\n"
-            + message_parsed
-            + "\n-------------------------END_MESSAGE-------------------------\n\n\n"
-        )
-
-        f.write("Receivers:\n")
-        for receiver in receivers:
-            f.write(str(receiver) + "\n")
-        f.write("\nFailed Receivers:\n")
-        for failedReceiver in failedReceivers:
-            f.write(str(failedReceiver) + "\n")
-        f.close()
-        print("log written succesfully")
-        return "log written successfully"
-    except Exception as e:
-        print("\nError:\n" + str(e) + "\n")
-        print("error writing log")
-        return "error writing log"
 
 
 # Declare a User Loader for Flask-Login.
@@ -177,7 +125,7 @@ def getEmailContent():
             recipients, subject, cc, message, variables
         )
         # Finish sending emails, render results in HTML
-        write_log(user, subject, message, receivers, failedReceivers)
+        writeLog.write_log(user, subject, cc, message, receivers, failedReceivers)
         return jsonify(
             success=1,
             output={"receivers": receivers, "failedReceivers": failedReceivers},
